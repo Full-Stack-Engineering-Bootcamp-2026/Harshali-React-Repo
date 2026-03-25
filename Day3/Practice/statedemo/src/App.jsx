6f4dfbd297e99c7152fa10b234a533a147c2bd2a
Harshali Patil <harshali.patil@mindbowser.com>
2026-03-25 09:24:12 +0530

    feat:Assignment3

--- Day3/Practice/statedemo/src/App.jsx
+++ Day3/Practice/statedemo/src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import Counter from './components/Counter'
+import Counter1 from './components/Counter1'
 function App() {
   
 
   return (
     <div>
         <Counter/>
+        <Counter1/>
     </div>
   )
 }
---
+++ Day3/Practice/statedemo/src/components/Counter1.jsx
@@ -0,0 +1,14 @@
+
+
+function Counter1(){
+
+   // handleClick =() =>{
+       // console.log("btn clicked")
+   // }
+    return (
+        <div>
+            <button onClick={()=>console.log("btn clicked")}>Add Animal</button>
+        </div>
+    )
+}
+export default Counter1
